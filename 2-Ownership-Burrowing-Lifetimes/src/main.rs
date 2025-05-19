37a49d215f0f8261d95359892c2be8f918e8fe54
Subash Acharya <hubert3158@gmail.com>
2025-05-19 02:13:07 -0400

    wip

--- 2-Ownership-Burrowing-Lifetimes/src/main.rs
+++ 2-Ownership-Burrowing-Lifetimes/src/main.rs
@@ -1,5 +1,4 @@
 use std::io::stdin;
-
 fn main() {
     text_indexer();
 }
@@ -25,23 +24,56 @@ fn main() {
 #[allow(dead_code)]
 fn text_indexer() {
     let mut paragraph = String::new();
-    let mut first_and_last_words: Vec<String> = Vec::new();
+    let mut no_of_lines = 0;
+    let mut first_and_last_words: Vec<(String, String)> = Vec::new();
+    let mut total_characters = 0;
+    let mut longest_word = "";
     loop {
         let mut input_line = String::new();
         stdin()
             .read_line(&mut input_line)
             .expect("Something went wrong");
+        if input_line.trim() == "exit" {
+            break;
+        }
+        no_of_lines += 1;
         add_to_paragraph(&mut paragraph, input_line.trim());
-        add_first_and_last_words(&mut first_and_last_words, String::from(input_line.trim()))
+        add_first_and_last_words(&mut first_and_last_words, String::from(input_line.trim()));
+        count_characters(input_line.trim(), &mut total_characters);
+    }
+    println!("Report");
+    println!("Total no of Lines: {}", no_of_lines);
+    for words_tuple in first_and_last_words {
+        println!("{}, {}", words_tuple.0, words_tuple.1);
+    }
+    println!("Total characters: {}", total_characters);
+    fn count_characters(input_line: &str, total_characters: &mut i32) {
+        input_line.chars().for_each(|c| {
+            if c != ' ' {
+                *total_characters += 1;
+            }
+        });
     }
 
     fn add_to_paragraph(paragraph: &mut String, input_line: &str) {
         let formatted_string = format!("{}{}", paragraph, input_line);
         *paragraph = formatted_string;
     }
-    fn add_first_and_last_words(words_list: &mut Vec<String>, input_line: String) {
-        // TODO: understand
+    fn add_first_and_last_words(words_list: &mut Vec<(String, String)>, input_line: String) {
         let words: Vec<_> = input_line.split(' ').collect();
-        words_list.push(input_line);
+        let words_size = words.len();
+        if words_size > 0 {
+            let first_letter: String = words
+                .get(0)
+                .expect("couldnt get the first item")
+                .to_string();
+            let last_letter: String = words
+                .get(words_size - 1)
+                .expect("couldnt get the first item")
+                .to_string();
+            words_list.push((first_letter, last_letter));
+        } else {
+            words_list.push((String::new(), String::new()));
+        }
     }
 }
